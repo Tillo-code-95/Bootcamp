cfe1c90d07efcfac2fa05b09b1be9b2948f24d50
tillo-code <tillorr10@gmail.com>
2021-04-19 13:57:18 +0200

    Intoducción a react, vemos los componentes

---
+++ Tema_6_react/cheatsheet/my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import FirstComponent from './components/FirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent></FirstComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ Tema_6_react/cheatsheet/my-app/src/components/FirstComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function FirstComponent() {
+    return (
+        <div>
+            <h3>Titulo</h3>
+            <small>16 agosto 2025</small>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+        </div>
+    )
+}
+
